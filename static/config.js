2af5a13b3740f25b994a2c9eecf785777ef707fa
Vidhya <83634591+Srividhya-Sainath@users.noreply.github.com>
2026-06-04 17:55:20 +0200

    Update config.js

--- static/config.js
+++ static/config.js
@@ -1,7 +1,7 @@
 window.PATHOLOGY_QA_CONFIG = {
   mode: "auto",
   manifestUrl: "./data/images.json",
-  hfImageBaseUrl: "",
+  hfImageBaseUrl: "https://huggingface.co/datasets/oyemainhun/benchmark/tree/main",
   staticPassword: "",
   annotationStoragePrefix: "pathologyQaAnnotations",
 };
